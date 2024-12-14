144c65329ac16cb4190da7b69c814a86cd878b93
Sedi Simpson <sedisimpson@Sedis-MacBook-Air.local>
2024-12-14 23:04:54 +0200

    ttb background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import ttbbackground from "./assets/ttbbackground.png";
 import "./App.css";
 
 function App() {
@@ -8,11 +9,7 @@ function App() {
 
   return (
     <>
-      <div style={{ textAlign: "center", padding: "20 px" }}>
-        <h1>Welcome to My React Website</h1>
-        <p>This is a simple website built with React.</p>
-        <button onClick={() => alert("You clicked me")}>Click Me</button>
-      </div>
+      <div className="hero-image"></div>
     </>
   );
 }
